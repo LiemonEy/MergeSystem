d7ba56671135803d9c951d0ad728eaf6021e8337
LiemonEy <basov-leha@mail.ru>
2026-05-26 23:34:42 +0500

    Update MergeManager.cs

--- MergeManager.cs
+++ MergeManager.cs
@@ -1,147 +1,149 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using YG;
 using static RocketAction;
 
 public class MergeManager : MonoBehaviour
 {
-    public static MergeManager instance;
+    public static MergeManager Instance;
+
+    [Header("Visuals")]
     public Sprite[] itemSprites;
     public Sprite[] bossSprites;
 
+    [Header("References")]
     [SerializeField] private BinScript binScript;
     [SerializeField] private GameObject prefabCell;
-    private List<MergeCell> mergeCells = new();
-    private int horizontalCellsCount = 5;
-    private int verticalCellsCount = 5;
+    [SerializeField] private CursorManager cursorManager;
+
+    [Header("Grid Settings")]
+    [SerializeField] private int horizontalCellsCount = 5;
+    [SerializeField] private int verticalCellsCount = 5;
+
+    private readonly List<MergeCell> mergeCells = new();
+    private readonly List<RocketAction> activeRockets = new();
+
+    private readonly int[] cellItemIds =
+    {
+        1, 1, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0
+    };
 
     public MergeCell enteredCell;
     public MergeCell draggingCell;
-    public CursorManager cursorManager;
 
-    [SerializeField] private List<RocketAction> actionList;
     public bool isWin = false;
 
-    private int[] itemIdList = { 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    private int rocketBuyedCount = 0;
     private int maxRocketId = 1;
 
     private void Awake()
     {
-        instance = this;
+        Instance = this;
     }
 
     public void Initialize()
     {
-        itemIdList = YG2.saves.itemIdList;
-        rocketBuyedCount = YG2.saves.rocketBuyedCount;
-        maxRocketId = itemIdList.Max();
+        maxRocketId = cellItemIds.Max();
 
-        if (UIManager.instance.GetLevel() < 5)        
-            verticalCellsCount = 2;        
-        else if (UIManager.instance.GetLevel() < 25)        
-            verticalCellsCount = 3;        
-        else if (UIManager.instance.GetLevel() < 65)        
-            verticalCellsCount = 4;        
-        else        
-            verticalCellsCount = 5;        
-
-        GetComponent<RectTransform>().localPosition -= new Vector3(0, (5 - verticalCellsCount) * 90, 0);
+        ConfigureGridSize();
 
         FillInventory();
         UpdateCells();
     }
 
-    private void OnDisable()
+    private void ConfigureGridSize()
     {
-        SetSaves();
-    }
+        int level = UIManager.instance.GetLevel();
 
-    public void SetSaves()
-    {
-        YG2.saves.itemIdList = GetItemIdList();
-        YG2.saves.rocketBuyedCount = rocketBuyedCount;
-    }
+        if (level < 5)
+            verticalCellsCount = 2;
+        else if (level < 25)
+            verticalCellsCount = 3;
+        else if (level < 65)
+            verticalCellsCount = 4;
+        else
+            verticalCellsCount = 5;
 
-    private int[] GetItemIdList()
-    {
-        for (int i = 0; i < verticalCellsCount * horizontalCellsCount; i++)
-        {
-            itemIdList[i] = mergeCells[i].itemId;
-        }
-        return itemIdList;
+        GetComponent<RectTransform>().localPosition -=
+            new Vector3(0, (5 - verticalCellsCount) * 90, 0);
     }
 
     private void FillInventory()
     {
-        for (int i = 0; i < verticalCellsCount * horizontalCellsCount; i++)
+        int totalCells = verticalCellsCount * horizontalCellsCount;
+
+        for (int i = 0; i < totalCells; i++)
         {
-            MergeCell cell = Instantiate(prefabCell, transform).GetComponent<MergeCell>();
+            MergeCell cell = Instantiate(prefabCell, transform)
+                .GetComponent<MergeCell>();
+
             mergeCells.Add(cell);
+
             cell.cellId = i;
-            cell.itemId = itemIdList[i];
+            cell.itemId = cellItemIds[i];
         }
     }
 
-    public void SpawnItem(List<MergeCell> freeMergeCells, int rocketId)
+    public void SpawnItem(List<MergeCell> freeCells, int rocketId)
     {
-        int index = UnityEngine.Random.Range(0, freeMergeCells.Count);
-        freeMergeCells[index].itemId += rocketId;
-        freeMergeCells[index].UpdateCell();
+        int randomIndex = Random.Range(0, freeCells.Count);
+
+        MergeCell targetCell = freeCells[randomIndex];
+
+        targetCell.itemId += rocketId;
+        targetCell.UpdateCell();
     }
 
-    public bool FreeCellsChecker()
+    public List<MergeCell> GetFreeCells()
     {
-        List<MergeCell> freeMergeCells = GetMergeCells();
+        List<MergeCell> freeCells = new();
 
-        if (freeMergeCells.Count > 0)
-            return true;
-        return false;
-    }
-
-    public List<MergeCell> GetMergeCells()
-    {
-        List<MergeCell> freeMergeCells = new();
         for (int i = 0; i < mergeCells.Count; i++)
         {
             if (mergeCells[i].itemId == 0)
             {
-                freeMergeCells.Add(mergeCells[i]);
+                freeCells.Add(mergeCells[i]);
             }
         }
-        return freeMergeCells;
+
+        return freeCells;
+    }
+
+    public bool HasFreeCells()
+    {
+        return GetFreeCells().Count > 0;
     }
 
     public void StartRockets()
     {
         for (int i = 0; i < mergeCells.Count; i++)
         {
-            RocketAction ra = mergeCells[i].GetComponentInChildren<RocketAction>();
-            if (ra != null)
-            {
-                ra.StartInitialize();
-                ra.SetState(RocketState.Flying);
-                actionList.Add(ra);
-            }
+            RocketAction rocket = mergeCells[i]
+                .GetComponentInChildren<RocketAction>();
+
+            if (rocket == null)
+                continue;
+
+            rocket.StartInitialize();
+            rocket.SetState(RocketState.Flying);
+
+            activeRockets.Add(rocket);
         }
     }
 
-    public void RemoveRocketObject(RocketAction ra)
+    public void RemoveRocket(RocketAction rocket)
     {
-        actionList.Remove(ra);
+        activeRockets.Remove(rocket);
 
-        if (actionList.Count == 0)
+        if (activeRockets.Count == 0)
+        {
             UIManager.instance.MenuActivate(isWin);
+        }
     }
 
-    public List<RocketAction> GetRocketList() => actionList;
-    
-    public void MaxRocketChecker(int id)
-    {
-        if (id > maxRocketId)
-            maxRocketId = id;
-    }
     public void UpdateCells()
     {
         for (int i = 0; i < mergeCells.Count; i++)
@@ -150,10 +152,26 @@ public class MergeManager : MonoBehaviour
         }
     }
 
+    public void CheckMaxRocket(int id)
+    {
+        if (id > maxRocketId)
+        {
+            maxRocketId = id;
+        }
+    }
+
     public bool CanDeleteRocket()
     {
         return binScript.isEntered;
     }
 
-    public int GetMaxRocket() => maxRocketId;
+    public int GetMaxRocket()
+    {
+        return maxRocketId;
+    }
+
+    public List<RocketAction> GetActiveRockets()
+    {
+        return activeRockets;
+    }
 }
